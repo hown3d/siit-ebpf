abbb6122b64de12d62bce06ff47facb8fed5b44b
Lukas Hoehl <lukas.hoehl@stackit.cloud>
2026-03-06 09:47:08 +0100

    cleanup scripts
    Signed-off-by: Lukas Hoehl <lukas.hoehl@stackit.cloud>

--- hack/bpftool.sh
+++ hack/bpftool.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
-if ! docker image ls | grep bpftool; then
-  docker build -t bpftool https://github.com/libbpf/bpftool.git
-fi
+if command -v bpftool; then
+  bpftool "$@"
+else
+  if ! docker image ls | grep bpftool; then
+    docker build -t bpftool https://github.com/libbpf/bpftool.git
+  fi
 
-docker run -v /sys/kernel:/sys/kernel bpftool "$@"
+  docker run -v /sys/kernel:/sys/kernel bpftool "$@"
+fi
